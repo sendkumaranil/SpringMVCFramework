d01cba0ebc55e9e300f08d51f1952ede45a2ba1f
Anil Kumar <sendkumaranil@gmail.com>
2016-07-04 11:06:18 +0530

    Add files via upload

---
+++ EmployeeMapper.java
@@ -0,0 +1,25 @@
+package com.springmvcexample.util;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.RowMapper;
+
+import com.springmvcexample.model.Employee;
+
+public class EmployeeMapper implements RowMapper<Employee> {
+
+	@Override
+	public Employee mapRow(ResultSet rs, int rownum) throws SQLException {
+		Employee employee=new Employee();
+		employee.setEmpid(rs.getInt("empid"));
+		employee.setEmpname(rs.getString("empname"));
+		employee.setEmailid(rs.getString("email"));
+		employee.setMobileno(rs.getString("mobileno"));
+		employee.setDob(rs.getString("birthdate"));
+		employee.setDoj(rs.getString("joindate"));
+		employee.setIdtype(rs.getString("idtype"));
+		employee.setIdno(rs.getString("idno"));
+		return employee;
+	}
+}
