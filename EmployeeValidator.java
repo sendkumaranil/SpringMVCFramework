1dfb6da223c07920779cdb142162e81129a7af60
Anil Kumar <sendkumaranil@gmail.com>
2016-07-04 11:06:33 +0530

    Add files via upload

---
+++ EmployeeValidator.java
@@ -0,0 +1,36 @@
+package com.springmvcexample.util;
+
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
+
+import com.springmvcexample.model.Employee;
+
+public class EmployeeValidator implements Validator {
+
+	@Override
+	public boolean supports(Class<?> param) {
+		
+		return Employee.class.equals(param);
+	}
+
+	@Override
+	public void validate(Object obj, Errors errors) {
+		
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "empname", "name.required");
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "emailid", "email.required");
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "mobileno", "phone.required");
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "dob", "dob.required");
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "doj", "doj.required");
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "idtype", "idtype.required");
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "idno", "idno.required");
+		
+		Employee employee=(Employee)obj;
+		if(!Utility.isValidEmail(employee.getEmailid())){
+			errors.rejectValue("emailid","invalidEmail",new Object[]{"emailid"},"Invalid emailid!!");
+		}
+		if(!Utility.validatePhoneNumber(employee.getMobileno())){
+			errors.rejectValue("mobileno","invalidmobileno",new Object[]{"mobileno"},"Invalid mobile No [only 10 digits allowed and no space and others characters allowed]");
+		}
+	}
+}
