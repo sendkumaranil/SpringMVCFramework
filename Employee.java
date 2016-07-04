15a723148b22362d976d43ed243453674a40b295
Anil Kumar <sendkumaranil@gmail.com>
2016-07-04 11:05:36 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,64 @@
+package com.springmvcexample.model;
+
+public class Employee {
+
+	private int empid;
+	private String empname;
+	private String emailid;
+	private String mobileno;
+	private String dob;
+	private String doj;
+	private String idtype;
+	private String idno;
+	
+	
+	public String getEmpname() {
+		return empname;
+	}
+	public void setEmpname(String empname) {
+		this.empname = empname;
+	}
+	public String getEmailid() {
+		return emailid;
+	}
+	public void setEmailid(String emailid) {
+		this.emailid = emailid;
+	}
+	public String getMobileno() {
+		return mobileno;
+	}
+	public void setMobileno(String mobileno) {
+		this.mobileno = mobileno;
+	}
+	public String getDob() {
+		return dob;
+	}
+	public void setDob(String dob) {
+		this.dob = dob;
+	}
+	public String getDoj() {
+		return doj;
+	}
+	public void setDoj(String doj) {
+		this.doj = doj;
+	}
+	public int getEmpid() {
+		return empid;
+	}
+	public void setEmpid(int empid) {
+		this.empid = empid;
+	}
+	public String getIdtype() {
+		return idtype;
+	}
+	public void setIdtype(String idtype) {
+		this.idtype = idtype;
+	}
+	public String getIdno() {
+		return idno;
+	}
+	public void setIdno(String idno) {
+		this.idno = idno;
+	}
+	
+}
