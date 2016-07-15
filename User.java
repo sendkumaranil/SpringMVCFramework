ad3d6c6e70ebea53e59b234cc347a6442096dc65
Anil Kumar <sendkumaranil@gmail.com>
2016-07-15 13:32:11 +0530

    Add files via upload

---
+++ User.java
@@ -0,0 +1,42 @@
+package com.springmvcexample.model;
+
+public class User {
+	
+	private int userid;
+	private String username;
+	private String password;
+	private String confirmPassword;
+	private String emailid;
+	
+	public int getUserid() {
+		return userid;
+	}
+	public void setUserid(int userid) {
+		this.userid = userid;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getConfirmPassword() {
+		return confirmPassword;
+	}
+	public void setConfirmPassword(String confirmPassword) {
+		this.confirmPassword = confirmPassword;
+	}
+	public String getEmailid() {
+		return emailid;
+	}
+	public void setEmailid(String emailid) {
+		this.emailid = emailid;
+	}
+	
+}
