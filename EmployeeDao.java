28bf53b6bd80399c0106c8798f5a7ad71c9d6728
Anil Kumar <sendkumaranil@gmail.com>
2016-07-15 13:31:03 +0530

    Add files via upload

--- EmployeeDao.java
+++ EmployeeDao.java
@@ -2,7 +2,11 @@ package com.springmvcexample.dao;
 
 import java.util.List;
 
+import com.springmvcexample.model.Department;
 import com.springmvcexample.model.Employee;
+import com.springmvcexample.model.Role;
+import com.springmvcexample.model.User;
+import com.springmvcexample.model.UserRoleMapping;
 
 public interface EmployeeDao {
 
@@ -11,4 +15,16 @@ public interface EmployeeDao {
 	public void addEmployee(Employee employee);
 	public void deleteEmployee(int empid);
 	public void editEmployee(Employee employee);
+	public void addUser(User user);
+	public void addRole(Role role);
+	public List<User> getUsers();
+	public List<Role> getRoles();
+	public void assignRoleToUser(int userid, int roleid);
+	public List<UserRoleMapping> getMappedUsers();
+	public String removeMappedUsers(UserRoleMapping userRoleMap);
+	public void resetPassword(User user);
+	public boolean isValidUser(User user);
+	public void changePassword(User user);
+	public String getRegisteredEmailid(User user);
+	public List<Department> getDepartments();
 }
